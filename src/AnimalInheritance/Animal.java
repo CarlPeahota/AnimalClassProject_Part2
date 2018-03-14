3336aef0b5fae4928879822068a37979d37f5f1d
cpeahota <cpeahota@afterhrs-3-surface.connexin.local>
2018-03-14 19:50:36 -0400

    new files added

---
+++ src/AnimalInheritance/Animal.java
@@ -0,0 +1,40 @@
+//Inteelisense suggested using @Override, but I'm not clear as to why, since it ran fine w/o it before the change was made
+
+package AnimalInheritance;
+
+class Animal {
+    public void makeSound() {}
+    public void cuteness() {}
+}
+class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+    @Override
+    public void cuteness() {
+        System.out.println("Very Cute!");
+    }    
+}
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+    @Override
+    public void cuteness() {
+        System.out.println("Cute!");
+    }    
+}
+
+class Program {
+    public static void main(String args[ ]) {
+        Animal a = new Dog();
+        Animal b = new Cat();
+        
+        a.makeSound();
+        a.cuteness();
+        b.makeSound();
+        b.cuteness();
+    }
+}
