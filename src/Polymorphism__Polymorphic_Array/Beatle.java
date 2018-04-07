f8604ecea540fa347292e25b8c4163de0035dd5f
cpeahota <cpeahota@afterhrs-3-surface.connexin.local>
2018-04-07 17:11:24 -0400

    Added the void eat() method

--- src/Polymorphism__Polymorphic_Array/Beatle.java
+++ src/Polymorphism__Polymorphic_Array/Beatle.java
@@ -1,5 +1,7 @@
 package Polymorphism__Polymorphic_Array;
 
 public class Beatle extends Insect {
-    
+    void eat() {
+    System.out.println("The Beatle already ate"); //This will override the superclass method
+    }
 }
