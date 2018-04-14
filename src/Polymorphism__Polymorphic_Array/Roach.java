c1712d613e8febfea655cb3b3568ab5373df93c2
cpeahota <cpeahota@afterhrs-3-surface.connexin.local>
2018-04-14 10:35:27 -0400

    added @Override

--- src/Polymorphism__Polymorphic_Array/Roach.java
+++ src/Polymorphism__Polymorphic_Array/Roach.java
@@ -2,6 +2,7 @@ package Polymorphism__Polymorphic_Array;
 
 public class Roach  extends Insect {
     
+    @Override
     void eat() {
     System.out.println("The Roach already ate"); //This will override the superclass method
     }
