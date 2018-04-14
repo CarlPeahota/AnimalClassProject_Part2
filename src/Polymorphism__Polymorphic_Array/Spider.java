66bd9a3a99c7b3a7fc2862ac605e4a54c1c3fba3
cpeahota <cpeahota@afterhrs-3-surface.connexin.local>
2018-04-14 10:34:50 -0400

    added @Override

--- src/Polymorphism__Polymorphic_Array/Spider.java
+++ src/Polymorphism__Polymorphic_Array/Spider.java
@@ -1,6 +1,7 @@
 package Polymorphism__Polymorphic_Array;
 
 public class Spider  extends Insect {
+    @Override
     void eat() {
     System.out.println("The Spider already ate"); //This will override the superclass method
     }    
