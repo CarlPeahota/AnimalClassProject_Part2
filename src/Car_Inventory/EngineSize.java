b3e3ab4dc200a042045b83ffcfcaa2c787d563db
cpeahota <cpeahota@afterhrs-3-surface.connexin.local>
2018-04-15 08:44:15 -0400

    new class

---
+++ src/Car_Inventory/EngineSize.java
@@ -0,0 +1,7 @@
+package Car_Inventory;
+
+//This will define the engine size for the type of car
+
+public class EngineSize {
+    
+}
---
+++ src/Car_Inventory/OilType.java
@@ -0,0 +1,7 @@
+package Car_Inventory;
+
+//This will define the type of oil for the type of car
+
+public class OilType {
+    
+}
